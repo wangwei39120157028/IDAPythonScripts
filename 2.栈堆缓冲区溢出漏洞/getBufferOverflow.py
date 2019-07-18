c2f93770a9902d2b885e948e2af61216971f8f26
wwy <39434325+wangwei39120157028@users.noreply.github.com>
2019-07-18 09:50:42 +0800

    Update getBufferOverflow.py

--- 2.栈堆缓冲区溢出漏洞/getBufferOverflow.py
+++ 2.栈堆缓冲区溢出漏洞/getBufferOverflow.py
@@ -6,10 +6,10 @@ from xml.dom.minidom import Document
 根据一个给定的XML Schema，使用DOM树的形式从空白文件生成一个XML
 '''
 
-doc = Document()  #创建DOM文档对象
+doc = Document()     # 创建DOM文档对象
 
-bookstore = doc.createElement('bookstore') #创建根元素
-bookstore.setAttribute('xmlns:xsi',"https://www.w3.org/2001/XMLSchema-instance")#设置命名空间
+bookstore = doc.createElement('bookstore')    # 创建根元素
+bookstore.setAttribute('xmlns:xsi',"https://www.w3.org/2001/XMLSchema-instance")    # 设置命名空间
 bookstore.setAttribute('xsi:noNamespaceSchemaLocation','bookstore.xsd')#引用本地XML Schema
 doc.appendChild(bookstore)
 
@@ -27,41 +27,41 @@ def is_stack_buffer(addr, idx):
    return get_stkvar(inst[idx], inst[idx].addr) != None 
 
 def find_arg(addr, arg_num):
-   # Get the start address of the function that we are in
+   # 获取函数所在段的起始地址
    function_head = GetFunctionAttr(addr, idc.FUNCATTR_START)    
    steps = 0
    arg_count = 0
-   # It is unlikely the arguments are 100 instructions away, include this as a safety check
-   while steps < 100:    
+   # 预计检查指令在80条以内
+   while steps < 80:    
        steps = steps + 1
-       # Get the previous instruction
+       # 向前查看指令
        addr = idc.PrevHead(addr)  
-       # Get the name of the previous instruction        
+       # 获取前一条指令的名称        
        op = GetMnem(addr).lower() 
-       # Check to ensure that we havent reached anything that breaks sequential code flow        
+       # 检查一下是否存在像ret,retn,jmp,b这样可以中断数据流的指令        
        if op in ("ret", "retn", "jmp", "b") or addr < function_head:            
            return
        if op == "push":
            arg_count = arg_count + 1
            if arg_count == arg_num:
-               #Return the operand that was pushed to the stack 
+               # 返回被push到堆栈的操作数
                return GetOpnd(addr, 0) 
 
-#漏洞名称及地址
+# 漏洞名称及地址
 vulnerabilities = doc.createElement('Vulnerabilities_StackOverflow')
 book.appendChild(vulnerabilities)
 
 price_size = 0
 print("Stack overflow vulnerabilities retrieval begins,Potential vulnerabilities will be printed below,Please wait...")
 for functionAddr in Functions():
-   # Check each function to look for strcpy
+   # 检查所有函数
    if "strcpy" in GetFunctionName(functionAddr): 
        xrefs = CodeRefsTo(functionAddr, False)
-       # Iterate over each cross-reference
+       # 遍历交叉引用，追踪函数执行过程
        for xref in xrefs:
-           # Check to see if this cross-reference is a function call
+           # 检查交叉引用是否是函数调用
            if GetMnem(xref).lower() == "call":
-               # Since the dest is the first argument of strcpy
+               # 找到函数的第一个参数
                opnd = find_arg(xref, 1) 
                function_head = GetFunctionAttr(xref, idc.FUNCATTR_START)
                addr = xref
@@ -72,9 +72,8 @@ for functionAddr in Functions():
                    if _op in ("ret", "retn", "jmp", "b") or _addr < function_head:
                        break
                    elif _op == "lea" and GetOpnd(_addr, 0) == opnd:
-                       # We found the destination buffer, check to see if it is in the stack
-                        if is_stack_buffer(_addr, 1):
-						
+                       # 检查目标函数的缓冲区是否在堆栈当中
+                        if is_stack_buffer(_addr, 1):		
                            author_addr  = doc.createElement('Vulnerability-addr')
                            author_addr_text  = doc.createTextNode(hex(addr))
                            vulnerabilities.appendChild(author_addr)
@@ -84,8 +83,7 @@ for functionAddr in Functions():
 						   
                            print "\n[+]STACK BUFFER STRCOPY FOUND at 0x%X"  % addr                        
                         break
-                   # If we detect that the register that we are trying to locate comes from some other register
-                   # then we update our loop to begin looking for the source of the data in that other register
+                   # 如果我们检测到要定位的寄存器是来自其他寄存器，则更新循环，在另一个寄存器中继续查找数据源
                    elif _op == "mov" and GetOpnd(_addr, 0) == opnd:
                        op_type = GetOpType(_addr, 1)
                        if op_type == o_reg:
@@ -96,7 +94,7 @@ for functionAddr in Functions():
    else:
        print "[-] No Stack overflow vulnerabilities found in " + GetFunctionName(functionAddr) + "."
 
-#这里写漏洞影响大小             
+# 这里写漏洞影响评分             
 price = doc.createElement('price')
 price_text = doc.createTextNode(str(price_size))
 price.appendChild(price_text)
